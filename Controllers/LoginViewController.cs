4555c2f088a2cddbcbe4c37902060dfd0fccd886
AndreAF23 <andretmx68@gmail.com>
2025-04-14 23:46:58 -0500

    Se agrega login seguro - incompleto

---
+++ Controllers/LoginViewController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prueba_DockerNET.Controllers
+{
+    public class LoginViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Login";
+            return View(); // Renderiza la vista Index.cshtml
+        }
+    }
+
+}
--- Data/Conexion.cs
+++ Data/Conexion.cs
@@ -13,6 +13,7 @@ namespace Prueba_DockerNET.Data
             //string connectionString = "Server=Prueba_NET_DOCKER.mssql.somee.com;Database=Prueba_NET_DOCKER;User Id=AndreAF23_SQLLogin_2;Password=2t3joratok;TrustServerCertificate=true;";
 
             //PARA DOCKER MANUAL CON .env
+            
             string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
             if (string.IsNullOrEmpty(connectionString))
                 throw new InvalidOperationException("La cadena de conexión está vacía o no inicializada.");
